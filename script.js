0a236b3450600c5f495827eafbcd17db6320bc89
ArdiMobile <34771129+ArdiMobile@users.noreply.github.com>
2026-04-20 20:45:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ dlForm.addEventListener('submit', async (e) => {
             </a>` : ""}
 
             <!-- AD -->
-         <div style="height: 500px;">
+         <div style="height: 60px;">
 <a href="https://www.yasing.com.et">
   <img src=" https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi3emvbxUKCk0RWnwHkvZsa-R9GSRca9bmPcokzxCElc7TPFnoErZcE9dlU9X3mCwXxUJdRB0qMWLRu3HcKpbMjPMBbvQ4KjGNCZGq658mW-2KaHHbSwcWD2JcbqjDhXrUJV9QyXeXwLtX7jgEEc6ZcGyE0WxTxwDKFgoovhQbl4I2wYQtFVMjqXoSzfCw/s320/20250.png " alt="Buy now">
 </a>
